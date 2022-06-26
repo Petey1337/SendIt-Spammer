ef5073c930d6080a69e4aa2d404a43c4a8fdf459
Petey <46226573+Petey1337@users.noreply.github.com>
2022-06-26 14:15:57 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,12 @@
 @echo off
-title SendIT Spammer -
+title [Petey's Sendit Spammer] -
 IF EXIST node_modules (
 goto start
 ) else (
 npm install
 )
 :start
+cls
 node --no-warnings index.js
 pause
+goto start
